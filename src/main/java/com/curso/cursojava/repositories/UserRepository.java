9effae5a71fce03dc406a4c1597dadff8b1d094b
Gabrielzaraki <gabriel.henrique.102@gmail.com>
2023-10-31 23:56:49 -0300

    Repositorio JPA, injeção de dependencia e inserindo dados no DB

---
+++ src/main/java/com/curso/cursojava/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.curso.cursojava.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.cursojava.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
