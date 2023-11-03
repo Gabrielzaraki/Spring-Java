684a5f1fc931be998f425835a6a1a919c5314662
Gabrielzaraki <gabriel.henrique.102@gmail.com>
2023-11-03 12:36:20 -0300

    Tratamento de exceção findById user

--- src/main/java/com/curso/cursojava/services/UserService.java
+++ src/main/java/com/curso/cursojava/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.curso.cursojava.entities.User;
 import com.curso.cursojava.repositories.UserRepository;
+import com.curso.cursojava.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findByID(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
