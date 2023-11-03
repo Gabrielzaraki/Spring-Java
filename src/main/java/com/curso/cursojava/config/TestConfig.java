d80a5d9eb0ae2cd0146f7dc1adc4b8aa3df5fd76
Gabrielzaraki <gabriel.henrique.102@gmail.com>
2023-11-03 11:02:44 -0300

    Payment associação um para um

--- src/main/java/com/curso/cursojava/config/TestConfig.java
+++ src/main/java/com/curso/cursojava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.curso.cursojava.entities.Category;
 import com.curso.cursojava.entities.Order;
 import com.curso.cursojava.entities.OrderItem;
+import com.curso.cursojava.entities.Payment;
 import com.curso.cursojava.entities.Product;
 import com.curso.cursojava.entities.User;
 import com.curso.cursojava.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
